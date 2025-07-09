961c0ede455d7eb77bc944f9344062f7674b4ab0
Maximilian Ehlert <maximilian.ehlert@student.htw-berlin.de>
2025-07-09 19:22:20 +0200

    refactoring & panorama with width of 5000 pixels

--- panorama.py
+++ panorama.py
@@ -4,40 +4,53 @@ from  PIL.Image import Transpose
 from pathlib import Path
 import numpy as np
 import cv2
+from PIL import ImageDraw 
+from PIL import ImageFont
+from tqdm import tqdm
+
+
 
 imgaes_path = Path("C:/Users/Maxim/Downloads/_PanoJPEG/_PanoJPEG").glob("*.jpg")
 index =0
-max_index = 1000
+max_index = 5000
 
 #values after rotation
 image_width = 1080
 image_width_center = image_width // 2
 image_height = 1920
 
-new_image_pix = []
 
+
+
+
+def create_panorama_image():
+    new_image_pix = []
+    index = 0
+    for image_path in tqdm(imgaes_path):
+        index+=1
+        img = cv2.imread(image_path)
+        new_image_pix.append( img[image_width_center, :])
+        if index == max_index:
+            return new_image_pix        
+
+def save_image(pixels_array, time_for_panorama_creation):
+    array = np.array(pixels_array, dtype=np.uint8)
+
+    panorama_image = Image.fromarray(array).transpose(Transpose.ROTATE_270)
+    draw = ImageDraw.Draw(panorama_image)
+    font = ImageFont.load_default(30)
+    draw.text((0, 0),f"time: {round(time_for_panorama_creation)} s",(0,0,0), font=font)
+    panorama_image.save('panorama.png')
+    panorama_image.show()
 
 t0 = time.time()
-
-
-for image_path in imgaes_path:
-    index+=1
-
-    img = cv2.imread(image_path)
-    new_image_pix.append( img[image_width_center, :])
-
-    if index == max_index:
-        break
-
+pixels_array = create_panorama_image()
 t1 = time.time()
-print(f"time: {t1-t0}")
-# https://stackoverflow.com/questions/46923244/how-to-create-image-from-a-list-of-pixel-values-in-python3
-# Convert the pixels into an array using numpy
-array = np.array(new_image_pix, dtype=np.uint8)
-# Use PIL to create an image from the new array of pixels
-panorama_image = Image.fromarray(array).transpose(Transpose.ROTATE_270)
-panorama_image.save('panorama.png')
-panorama_image.show()
+time_for_panorama_creation = t1-t0
+save_image(pixels_array, time_for_panorama_creation)
+
+
+
 
 
     
